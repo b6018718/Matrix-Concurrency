cdd5c73655476d2acaf55ae8e73b64b1a4bef6f4
Anthony Dranfield <34162989+b6018718@users.noreply.github.com>
2019-10-14 13:59:49 +0100

    There github

---
+++ Matrix Multiplication Concurrency/Matrix.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <vector>
+#include <string>
+using namespace std;
+
+class Matrix
+{
+public:
+	// Use filename to build matrix
+	Matrix(string filename);
+	// Use vector to build matrix
+	Matrix(vector <vector<int>> mVector);
+	// Create an empty array with a size
+	Matrix(int height, int width);
+
+	~Matrix();
+
+	vector <vector<int>> mVector;
+	
+	// Getters
+	size_t height();
+	size_t width();
+
+	// Functions
+	void save(string filename);
+
+private:
+	vector<int> split(string str, char delimiter);
+};
